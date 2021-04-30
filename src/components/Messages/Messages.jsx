59064f32e5b6e935d9f26d694062d20c8f4e03f5
Rustam <avaraa@mail.ru>
2021-04-30 12:16:48 +0300

    add .map for message

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -17,24 +17,32 @@ const Messages = () => {
         {id: 2, item: "item2"},
         {id: 3, item: "item3"},
         {id: 4, item: "item4"},
-    ]
+    ];
+    // создали метод для перебора масива. 
+    // если функция принимает один параметр, можем писать его без скобок.
+    // если return возвращает одну строку, то можно его не прописывать.
+    let MesDataNameEl = MesDataName.map(name => (<MessageName name={name.name} id={name.id} />));
+    let MesDataItemEl = MesDataItem.map(item => (<MessageItem item={item.item} id={item.id} />))
+
     return (
         <div className={ModCSS.messages}>
             <div className={ModCSS.name}>
+                { MesDataNameEl }
                 {/* в этом способе данные берем из масива с объектами */}
-                <MessageName name={MesDataName[0].name} id={MesDataName[0].id} />
-                <MessageName name={MesDataName[1].name} id={MesDataName[1].id} />
+                {/* <MessageName name={MesDataName[0].name} id={MesDataName[0].id} />
+                <MessageName name={MesDataName[1].name} id={MesDataName[1].id} /> */}
                 {/* передаём атрибуты компоненте, через пропсы */}
-                <MessageName name="name3" id="3" />
-                <MessageName name="name4" id="4" />
+                {/* <MessageName name="name3" id="3" />
+                <MessageName name="name4" id="4" /> */}
             </div>
             <div className={ModCSS.item}>
+                { MesDataItemEl }
                 {/* в этом способе данные берем из масива с объектами */}
-               <MessageItem item={MesDataItem[0].item} id={MesDataItem[0].id} />
-               <MessageItem item={MesDataItem[1].item} id={MesDataItem[1].id} />
+                {/* <MessageItem item={MesDataItem[0].item} id={MesDataItem[0].id} />
+                <MessageItem item={MesDataItem[1].item} id={MesDataItem[1].id} /> */}
                 {/* передаём атрибуты компоненте, через пропсы */}
-                <MessageItem item="item3" />
-               <MessageItem item="item4" />
+                {/* <MessageItem item="item3" />
+               <MessageItem item="item4" /> */}
             </div>
         </div>
     );
